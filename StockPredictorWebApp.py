81af0b1b8c1384a122699be7e45f3031e6c49083
DelRiscoAriel <69070833+DelRiscoAriel@users.noreply.github.com>
2024-07-08 17:02:30 -0400

    Update StockPredictorWebApp.py

--- StockPredictorWebApp.py
+++ StockPredictorWebApp.py
@@ -2,7 +2,7 @@
 import streamlit as st 
 from datetime import date
 import yfinance as yf
-from prophet.forecaster import Prophet
+from prophet import Prophet
 from prophet.plot import plot_plotly
 from plotly import graph_objs as go
 
